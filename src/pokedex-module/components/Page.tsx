4738ccbd5666baa337150679b9ca002945d962c4
Manuel Cañero Sancho <manuelcasancho@gmail.com>
2022-07-01 17:25:11 +0200

    added tweak on page component

--- src/pokedex-module/components/Page.tsx
+++ src/pokedex-module/components/Page.tsx
@@ -1,14 +1,33 @@
 import styled from "@emotion/styled";
 import { FC } from "react";
+import theme from "../../theme";
 
 const Page: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <Layout>{children}</Layout>;
+  return (
+    <Layout>
+      <Container>{children}</Container>
+    </Layout>
+  );
 };
 
 const Layout = styled.div`
+  font-family: "VT323";
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+const Container = styled.div`
+  background-color: ${theme.palette.red.background};
+  padding: ${theme.spacing(5)};
+  border: 5px double;
+  border-radius: ${theme.spacing(3)};
+  margin: ${theme.spacing(4)};
+  height: calc(100% - ${theme.spacing(5)});
+  width: 60%;
+  ${theme.tablet} {
+    width: 100%;
+  }
 `;
 
 export default Page;
